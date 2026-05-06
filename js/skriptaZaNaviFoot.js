dab489b12359f19d0026b3704f9968b263b1e837
labadikristina-hub <labadikristina@gmail.com>
2026-05-06 18:38:37 +0200

    Initial commit Avip Solutions

---
+++ js/skriptaZaNaviFoot.js
@@ -0,0 +1,16 @@
+
+function ucitajElemente() {
+  fetch('nav.html')
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById('nav-placeholder').innerHTML = data;
+    });
+
+  fetch('footer.html')
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById('footer-placeholder').innerHTML = data;
+    });
+}
+
+ucitajElemente();
